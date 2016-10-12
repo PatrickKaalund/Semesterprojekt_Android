9d5a394168e21a1cdb668bdf1075678a8cfa75c3
hippomormor <cgenter@oncable.dk>
2016-10-13 00:27:27 +0200

    Added music service Added top bar options (main menu)

--- app/src/main/java/activities/MainMenu.java
+++ app/src/main/java/activities/MainMenu.java
@@ -1,15 +1,108 @@
 package activities;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.patrickkaalund.semesterprojekt_android.R;
+import services.MusicService;
 
 public class MainMenu extends AppCompatActivity {
+    boolean musicIsBound = false;
+    MusicService musicService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        // Add music boolean to preference manager if not present
+        if (!preferences.contains("music")) {
+            preferences.edit()
+                    .putBoolean("music", false) // Music disabled by default
+                    .apply();
+        }
+        //preferences.edit().putBoolean("music", true).apply();     // Enable music (debug)
+        doBindService();
+    }
+
+    // Kunne undlades..
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.top_bar_options_main, menu);
+        return true;
+    }
+
+    // Kunne undlades..
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.options) {
+            // Todo Implement options
+            Toast.makeText(this, "Not implemented", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == R.id.quit)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        doUnbindService();
+        super.onDestroy();
+    }
+
+    // Handle music when Home is pressed
+    @Override
+    protected void onPause() {
+        if(musicIsBound) {
+            musicService.pauseMusic();
+            musicIsBound = false;
+        }
+        super.onPause();
+    }
+
+    // Handle music when app is resumed
+    @Override
+    protected void onPostResume() {
+        // Check if music is enabled in preferences (default false)
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if(!musicIsBound && preferences.getBoolean("music", false)) {
+            musicService.resumeMusic();
+            musicIsBound = true;
+        }
+        super.onPostResume();
+    }
+
+    ServiceConnection serviceConnection = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+            musicService = ((MusicService.ServiceBinder) binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            musicService = null;
+        }
+    };
+
+    void doBindService() {
+        bindService(new Intent(this,MusicService.class),
+                serviceConnection, Context.BIND_AUTO_CREATE);
+        musicIsBound = true;
+    }
+
+    void doUnbindService() {
+        if(musicIsBound) {
+            unbindService(serviceConnection);
+            musicIsBound = false;
+        }
     }
 }
---
+++ app/src/main/java/services/MusicService.java
@@ -0,0 +1,102 @@
+package services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.example.patrickkaalund.semesterprojekt_android.R;
+
+public class MusicService extends Service implements MediaPlayer.OnErrorListener {
+
+    private final IBinder mediaBinder;
+    private MediaPlayer mediaPlayer;
+
+    public MusicService() {
+        mediaBinder = new ServiceBinder();
+    }
+
+    public class ServiceBinder extends Binder {
+        public MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return mediaBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        mediaPlayer.setOnErrorListener(this);
+
+        if (mediaPlayer != null) {
+            mediaPlayer.setLooping(true);
+            mediaPlayer.setVolume(0.4f, 0.4f);
+        }
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (preferences.getBoolean("music", true))
+            mediaPlayer.start();
+
+        mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                onError(mediaPlayer, what, extra);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            try {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+            } finally {
+                mediaPlayer = null;
+            }
+        }
+    }
+
+    public void pauseMusic() {
+        if(mediaPlayer.isPlaying())
+            mediaPlayer.pause();
+    }
+
+    public void resumeMusic() {
+        if(!mediaPlayer.isPlaying())
+            mediaPlayer.start();
+    }
+
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+
+        Log.d("ERROR", "Music player failed");
+
+        if (mediaPlayer != null) {
+            try {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+            } finally {
+                mediaPlayer = null;
+            }
+        }
+        return false;
+    }
+
+
+}
