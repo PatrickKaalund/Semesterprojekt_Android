2906616e1c8a51b14d9d96b13610cb76e834f53c
Hippomormor <cgenter@oncable.dk>
2016-10-13 19:11:25 +0200

    Handling pause/resume for clock

--- app/src/main/java/com/activities/InGame.java
+++ app/src/main/java/com/activities/InGame.java
@@ -1,7 +1,5 @@
 package com.activities;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
