f693a09a1d191c40bf27476ede9fc3ff64ffa9a2
thor <send.tilmig@gmail.com>
2016-10-14 03:00:03 +0200

    added background to map. Should consider a changing ScreenDrawer to  SurficeView instead of just view

--- app/src/main/java/com/gamelogic/Map.java
+++ app/src/main/java/com/gamelogic/Map.java
@@ -2,46 +2,105 @@ package com.gamelogic;
 
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
 
 import com.core.GDrawable;
 import com.core.ScreenDrawer;
+import com.example.patrickkaalund.semesterprojekt_android.R;
 
 public class Map extends GDrawable {
 
+    private BitmapDrawable backgrundImage;
 
-
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public Map(Context context, ScreenDrawer screenDrawer) {
         super(context, screenDrawer);
         game.objectsToUpdate.add(this);
         screenDrawer.objectsToDraw.add(this);
-
+        Bitmap bmp = BitmapFactory.decodeResource(context.getResources(), R.drawable.backgrounddetailed2);
+        backgrundImage = new BitmapDrawable(context.getResources(), bmp);
+        backgrundImage.setTileModeXY(Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
     }
 
+    int increase = 0;
 
     @Override
     public void draw(Canvas canvas) {
-
+        Rect imageBounds = canvas.getClipBounds();
+        backgrundImage.setBounds(imageBounds);
+        backgrundImage.draw(canvas);
     }
 
     @Override
     public void update() {
-
+        increase--;
     }
 
+
+}
+
+
+//package com.gamelogic;
+//
+//
+//        import android.content.Context;
+//        import android.graphics.Bitmap;
+//        import android.graphics.BitmapFactory;
+//        import android.graphics.Canvas;
+//        import android.graphics.Rect;
+//        import android.graphics.drawable.BitmapDrawable;
+//        import android.graphics.drawable.Drawable;
+//        import android.os.Build;
+//        import android.support.annotation.RequiresApi;
+//        import android.util.AttributeSet;
+//
+//        import com.core.GDrawable;
+//        import com.core.ScreenDrawer;
+//        import com.example.patrickkaalund.semesterprojekt_android.R;
+//
+//public class Map extends GDrawable {
+//
+//    private Drawable backgrundImage;
+//
+//    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+//    public Map(Context context, ScreenDrawer screenDrawer) {
+//        super(context, screenDrawer);
+//        game.objectsToUpdate.add(this);
+//        screenDrawer.objectsToDraw.add(this);
+//        Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.bg_image);
+//        BitmapDrawable bitmapDrawable = new BitmapDrawable(bmp);
+//        bitmapDrawable.setTileModeXY(Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+//        backgrundImage = context.getResources().getDrawable(R.drawable.backgrounddetailed2, null);
+//        backgrundImage.setTileModeXY
+//    }
+//
+//    int increase = 0;
+//
+//    @Override
+//    public void draw(Canvas canvas) {
+//        Rect imageBounds = canvas.getClipBounds();
+//        imageBounds.right += increase;
+//        imageBounds.left += increase;
+//        imageBounds.top += increase;
+//        imageBounds.bottom += increase;
+//        imageBounds.union(imageBounds);
+//        backgrundImage.setBounds(imageBounds);
+//        backgrundImage.draw(canvas);
+//    }
+//
 //    @Override
 //    public void update() {
-//
+//        increase--;
 //    }
 //
-//    @Override
-//    public void draw(Canvas canvas, Paint paint, Context context) {
-
-        //Log.d("Map","drawing map");
-
-       // paint.setColor(Color.BLUE);
-
-      //  canvas.drawCircle(xPosition, yPosition, 15, paint);
-
-//    }
-}
+//
+//}
