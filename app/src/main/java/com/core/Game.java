c998edb00ebf2031dc34db2cde73448e52b2d176
hippomormor <cgenter@oncable.dk>
2016-10-28 01:49:01 +0200

    Added setInventoryButton Added delay when paused to save CPU

--- app/src/main/java/com/core/Game.java
+++ app/src/main/java/com/core/Game.java
@@ -1,6 +1,7 @@
 package com.core;
 
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Handler;
 import android.util.Log;
@@ -11,6 +12,7 @@ import com.gamelogic.Control;
 import com.gamelogic.Player;
 import com.gamelogic.Map;
 import com.graphics.OurGLSurfaceView;
+import com.views.DropDownMenu;
 
 import java.util.ArrayList;
 
@@ -27,7 +29,6 @@ public class Game implements Runnable {
     private boolean isPaused;
     private Control control;
     private FPSDrawer fpsDrawer;
-
     private int latestFPS = 0;
 //    private Collision collision;
 
@@ -39,6 +40,7 @@ public class Game implements Runnable {
 
     public Game(Context context) {
         GUpdateable.game = this;
+
         gl = new OurGLSurfaceView(context);
 
         this.context = context;
@@ -48,7 +50,7 @@ public class Game implements Runnable {
 //        map = new Map(context, screenDrawer);
 //        fpsDrawer = new FPSDrawer(context, screenDrawer);
 //        player = new Player(context, screenDrawer);
-        control = new Control(context);
+        control = new Control(context, this);
 //        collision = new Collision(map);
 
         // 30Hz clock
@@ -59,7 +61,6 @@ public class Game implements Runnable {
         thread.start();
 
         fpsMeasuring.start();
-
     }
 
     public void setJoystick(JoystickView joystickView) {
@@ -68,6 +69,10 @@ public class Game implements Runnable {
 
     public void setShootButton(FloatingActionButton shootButton) {
         control.setShootButton(shootButton);
+    }
+
+    public void setInventoryButton(DropDownMenu dropDownMenu) {
+        control.setInventoryButton(dropDownMenu);
     }
 
     // Running thread with 30 Hz
@@ -79,6 +84,13 @@ public class Game implements Runnable {
                 update();
 //                screenDrawer.draw();
                 fpsMeasuring.counter++;
+            } else { try {
+                    Log.d("Game", "Clock is Paused!");
+                    // Slow loop down when paused to save CPU
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
 
 //            handler.postDelayed(this, 33);
