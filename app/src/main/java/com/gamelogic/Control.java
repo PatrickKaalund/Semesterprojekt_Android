457485fc448620ba1cb2b31cc1c14e874eff9f3f
hippomormor <cgenter@oncable.dk>
2016-10-28 01:50:08 +0200

    Implemented DropDownMenu Handled pause

--- app/src/main/java/com/gamelogic/Control.java
+++ app/src/main/java/com/gamelogic/Control.java
@@ -1,19 +1,32 @@
 package com.gamelogic;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+
+import com.activities.OptionsActivity;
+import com.core.Game;
+import com.views.DropDownMenu;
 
 import java.util.ArrayList;
 
 import io.github.controlwear.virtual.joystick.android.JoystickView;
 
+import static com.views.DropDownMenu.STATE_PRESSED;
+import static java.security.AccessController.getContext;
+
 public class Control {
 
-    public ArrayList<Integer> joystickValues;
-
-    public Control(Context context){
+    private ArrayList<Integer> joystickValues;
+    private Game game;
+    Context context;
+    public Control(Context context, Game game){
+        this.context = context;
+        this.game = game;
         Log.d("Joystick", "started");
 
         joystickValues = new ArrayList<>();
@@ -42,6 +55,39 @@ public class Control {
                 Log.d("Shoot_button", "Bang!");
             }
         });
+    }
+
+    public void setInventoryButton(final DropDownMenu dropDownMenu) {
+       dropDownMenu.setOnTouchListener(new View.OnTouchListener() {
+           @Override
+           public boolean onTouch(View v, MotionEvent event) {
+               if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                   int menuItem = dropDownMenu.isMenuItemTouched(event);
+                   if (dropDownMenu.isMenuVisible && menuItem > 0) {
+                       if (menuItem <= dropDownMenu.mDrawableArray.size()) {
+                           dropDownMenu.mDrawableArray.get(dropDownMenu.mMenuPoints.size() - menuItem).setState(STATE_PRESSED);
+                           dropDownMenu.invalidate();
+                           if (menuItem == 1) {
+                               Log.d("DropDownPressed", "Options");
+                               dropDownMenu.closeMenu();
+                               Intent settings = new Intent(context, OptionsActivity.class);
+                               ((Activity) context).startActivity(settings);
+                           } else if (menuItem == 2) {
+                               Log.d("DropDownPressed", "Rifle");
+                               dropDownMenu.closeMenu();
+                           } else if (menuItem == 3) {
+                               Log.d("DropDownPressed", "Sidearm");
+                               dropDownMenu.closeMenu();
+                           }
+                       } return true;
+                   }
+               } return false;
+           }
+       });
+    }
+
+    public void gameStart() {
+        game.gameStart();
     }
 
     public ArrayList<Integer> getJoystickValues(){
