532a47e1e041c0b1460a60b874af17f368723d83
Hippomormor <cgenter@oncable.dk>
2016-11-25 00:34:11 +0100

    Added sound to weapon switch

--- app/src/main/java/com/gamelogic/Control.java
+++ app/src/main/java/com/gamelogic/Control.java
@@ -3,6 +3,9 @@ package com.gamelogic;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -10,6 +13,7 @@ import android.view.View;
 
 import com.activities.OptionsActivity;
 import com.core.Game;
+import com.example.patrickkaalund.semesterprojekt_android.R;
 import com.views.DropDownMenu;
 
 import java.util.ArrayList;
@@ -23,14 +27,14 @@ public class Control {
     private ArrayList<Integer> joystickValues;
     private Game game;
     private Context context;
-
-
+    private SharedPreferences preferences;
 
     private boolean shooting = false;
 
-    public Control(Context context, Game game){
+    public Control(Context context, Game game) {
         this.context = context;
         this.game = game;
+        preferences = PreferenceManager.getDefaultSharedPreferences(context);
         Log.d("Joystick", "started");
 
         joystickValues = new ArrayList<>();
@@ -58,6 +62,7 @@ public class Control {
         shootButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     Log.d("ShootPressed", "Shooting");
                     shooting = true;
@@ -73,6 +78,7 @@ public class Control {
                         Log.d("MoveEventX", "Down");
                     else if (lastX > event.getAxisValue(MotionEvent.AXIS_Y))
                         Log.d("MoveEventX", "Up");
+
                     lastX = event.getAxisValue(MotionEvent.AXIS_X);
                     lastY = event.getAxisValue(MotionEvent.AXIS_Y);
                 }
@@ -80,80 +86,111 @@ public class Control {
             }
         });
     }
+
     public boolean isShooting() {
         return shooting;
     }
 
     public void setInventoryButton(final DropDownMenu dropDownMenu) {
-       dropDownMenu.setOnTouchListener(new View.OnTouchListener() {
-           @Override
-           public boolean onTouch(View v, MotionEvent event) {
-               if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                   int menuItem = dropDownMenu.isMenuItemTouched(event);
-                   if (dropDownMenu.isMenuVisible && menuItem > 0) {
-                       if (menuItem <= dropDownMenu.mDrawableArray.size()) {
-                           dropDownMenu.mDrawableArray.get(dropDownMenu.mMenuPoints.size() - menuItem).setState(STATE_PRESSED);
-                           dropDownMenu.invalidate();
-                           if (menuItem == 1) {
-                               Log.d("DropDownPressed", "Options");
-                               dropDownMenu.closeMenu();
-                               Intent settings = new Intent(context, OptionsActivity.class);
-                               ((Activity) context).startActivity(settings);
-                           } else if (menuItem == 2) {
-                               Log.d("DropDownPressed", "Rifle");
-                               dropDownMenu.closeMenu();
-                               game.getPlayer().getEntity().setAnimationOrder(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19});
-                               game.getPlayer().setCurrentWeapon(2);
-                               int currentSprite = game.getPlayer().getEntity().getCurrentSprite();
-                               if (game.getPlayer().getCurrentWeapon() == 0) {
-                                   game.getPlayer().getEntity().setCurrentSprite(currentSprite - 46);
-                               } else if (game.getPlayer().getCurrentWeapon() == 1) {
-                                   game.getPlayer().getEntity().setCurrentSprite(currentSprite - 23);
-                               } else if (game.getPlayer().getCurrentWeapon() == 2) {
-                                   game.getPlayer().getEntity().setCurrentSprite(currentSprite);
-                               }
-                               game.map.move(0, 0);
-                           } else if (menuItem == 3) {
-                               Log.d("DropDownPressed", "Shotgun");
-                               dropDownMenu.closeMenu();
-                               game.getPlayer().getEntity().setAnimationOrder(new int[]{23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42});
-                               game.getPlayer().setCurrentWeapon(1);
-                               int currentSprite = game.getPlayer().getEntity().getCurrentSprite();
-                               if (game.getPlayer().getCurrentWeapon() == 0) {
-                                   game.getPlayer().getEntity().setCurrentSprite(currentSprite - 23);
-                               } else if (game.getPlayer().getCurrentWeapon() == 1) {
-                                   game.getPlayer().getEntity().setCurrentSprite(currentSprite);
-                               } else if (game.getPlayer().getCurrentWeapon() == 2) {
-                                   game.getPlayer().getEntity().setCurrentSprite(currentSprite + 23);
-                               }
-                               game.map.move(0, 0);
-                           } else if (menuItem == 4) {
-                               Log.d("DropDownPressed", "Sidearm");
-                               dropDownMenu.closeMenu();
-                               game.getPlayer().getEntity().setAnimationOrder(new int[]{45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64});
-                               game.getPlayer().setCurrentWeapon(0);
-                               int currentSprite = game.getPlayer().getEntity().getCurrentSprite();
-                               if (game.getPlayer().getCurrentWeapon() == 0) {
-                                   game.getPlayer().getEntity().setCurrentSprite(currentSprite);
-                               } else if (game.getPlayer().getCurrentWeapon() == 1) {
-                                   game.getPlayer().getEntity().setCurrentSprite(currentSprite + 23);
-                               } else if (game.getPlayer().getCurrentWeapon() == 2) {
-                                   game.getPlayer().getEntity().setCurrentSprite(currentSprite + 46);
-                               }
-                               game.map.move(0, 0);
-                           }
-                       } return true;
-                   }
-               } return false;
-           }
-       });
+        dropDownMenu.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                    int menuItem = dropDownMenu.isMenuItemTouched(event);
+                    if (dropDownMenu.isMenuVisible && menuItem > 0) {
+
+                        if (menuItem <= dropDownMenu.mDrawableArray.size()) {
+                            dropDownMenu.mDrawableArray.get(dropDownMenu.mMenuPoints.size() - menuItem).setState(STATE_PRESSED);
+                            dropDownMenu.invalidate();
+
+                            if (menuItem == 1) {
+                                Log.d("DropDownPressed", "Options");
+                                dropDownMenu.closeMenu();
+                                Intent settings = new Intent(context, OptionsActivity.class);
+                                context.startActivity(settings);
+                            } else if (menuItem == 2) {
+                                Log.d("DropDownPressed", "Rifle");
+                                dropDownMenu.closeMenu();
+
+                                if (preferences.getBoolean("sound", true)) {
+                                    MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.reload);
+                                    mediaPlayer.start();
+                                }
+
+                                game.getPlayer().getEntity().setAnimationOrder(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19});
+                                game.getPlayer().setCurrentWeapon(2);
+
+                                int currentSprite = game.getPlayer().getEntity().getCurrentSprite();
+                                if (game.getPlayer().getCurrentWeapon() == 0) {
+                                    game.getPlayer().getEntity().setCurrentSprite(currentSprite - 46);
+                                } else if (game.getPlayer().getCurrentWeapon() == 1) {
+                                    game.getPlayer().getEntity().setCurrentSprite(currentSprite - 23);
+                                } else if (game.getPlayer().getCurrentWeapon() == 2) {
+                                    game.getPlayer().getEntity().setCurrentSprite(currentSprite);
+                                }
+
+                                game.map.move(0, 0);
+                            } else if (menuItem == 3) {
+                                Log.d("DropDownPressed", "Shotgun");
+                                dropDownMenu.closeMenu();
+
+                                if (preferences.getBoolean("sound", true)) {
+                                    MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.reload);
+                                    mediaPlayer.start();
+                                }
+
+                                game.getPlayer().getEntity().setAnimationOrder(new int[]{23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42});
+                                game.getPlayer().setCurrentWeapon(1);
+
+                                int currentSprite = game.getPlayer().getEntity().getCurrentSprite();
+                                if (game.getPlayer().getCurrentWeapon() == 0) {
+                                    game.getPlayer().getEntity().setCurrentSprite(currentSprite - 23);
+                                } else if (game.getPlayer().getCurrentWeapon() == 1) {
+                                    game.getPlayer().getEntity().setCurrentSprite(currentSprite);
+                                } else if (game.getPlayer().getCurrentWeapon() == 2) {
+                                    game.getPlayer().getEntity().setCurrentSprite(currentSprite + 23);
+                                }
+
+                                game.map.move(0, 0);
+                            } else if (menuItem == 4) {
+                                Log.d("DropDownPressed", "Sidearm");
+                                dropDownMenu.closeMenu();
+
+                                if (preferences.getBoolean("sound", true)) {
+                                    MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.reload);
+                                    mediaPlayer.start();
+                                }
+
+                                game.getPlayer().getEntity().setAnimationOrder(new int[]{45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64});
+                                game.getPlayer().setCurrentWeapon(0);
+
+                                int currentSprite = game.getPlayer().getEntity().getCurrentSprite();
+                                if (game.getPlayer().getCurrentWeapon() == 0) {
+                                    game.getPlayer().getEntity().setCurrentSprite(currentSprite);
+                                } else if (game.getPlayer().getCurrentWeapon() == 1) {
+                                    game.getPlayer().getEntity().setCurrentSprite(currentSprite + 23);
+                                } else if (game.getPlayer().getCurrentWeapon() == 2) {
+                                    game.getPlayer().getEntity().setCurrentSprite(currentSprite + 46);
+                                }
+
+                                game.map.move(0, 0);
+                            }
+                        }
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 
     public void gameStart() {
         game.gameStart();
     }
 
-    public ArrayList<Integer> getJoystickValues(){
+    public ArrayList<Integer> getJoystickValues() {
         return this.joystickValues;
     }
 }
