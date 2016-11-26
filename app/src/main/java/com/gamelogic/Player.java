46dc4f08c1f9ba99d89bfd178d95b8ac7d6f3010
Hippomormor <cgenter@oncable.dk>
2016-11-26 18:33:07 +0100

    Cleanup

--- app/src/main/java/com/gamelogic/Player.java
+++ app/src/main/java/com/gamelogic/Player.java
@@ -381,12 +381,12 @@ public class Player extends PlayerCommon {
         }
 
         int currentSprite = player.getCurrentSprite();
-        switch (getCurrentWeapon()) {
+        switch (currentWeapon) {
 
             case GUN:
                 player.setAnimationOrder(new int[]{45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64});
                 
-                switch (getCurrentWeapon()) {
+                switch (currentWeapon) {
                     case GUN:
                         player.setCurrentSprite(currentSprite);
                         break;
@@ -402,7 +402,7 @@ public class Player extends PlayerCommon {
             case SHOTGUN:
                 player.setAnimationOrder(new int[]{23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42});
 
-                switch (getCurrentWeapon()) {
+                switch (currentWeapon) {
                     case GUN:
                         player.setCurrentSprite(currentSprite - 23);
                         break;
@@ -418,7 +418,7 @@ public class Player extends PlayerCommon {
             case AK47:
                 player.setAnimationOrder(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19});
 
-                switch (getCurrentWeapon()) {
+                switch (currentWeapon) {
                     case GUN:
                         player.setCurrentSprite(currentSprite - 46);
                         break;
