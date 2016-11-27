866c62af2526c04e77368fe1ed127a0d94d3ccb7
Hippomormor <cgenter@oncable.dk>
2016-11-27 02:10:26 +0100

    Added animations to menus

--- app/src/main/java/com/activities/MainMenu.java
+++ app/src/main/java/com/activities/MainMenu.java
@@ -2,12 +2,12 @@ package com.activities;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.RelativeLayout;
@@ -72,21 +72,26 @@ public class MainMenu extends BaseActivity implements View.OnClickListener {
     public void onClick(View v) {
         if (firstRun) {
             RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.RLayout);
+            relativeLayout.startAnimation(AnimationUtils.loadAnimation(this, R.anim.startup));
             relativeLayout.setVisibility(View.VISIBLE);
+            audioPlayer.playAudioFromRaw(R.raw.baretta);
             firstRun = false;
         } else {
             switch (v.getId()) {
                 case R.id.buttonPlay:
+                    v.startAnimation(AnimationUtils.loadAnimation(this, R.anim.view_clicked));
                     audioPlayer.playAudioFromRaw(R.raw.click);
                     Intent play = new Intent(this, InGame.class);
                     startActivity(play);
                     break;
                 case R.id.buttonSettings:
+                    v.startAnimation(AnimationUtils.loadAnimation(this, R.anim.view_clicked));
                     audioPlayer.playAudioFromRaw(R.raw.click);
                     Intent settings = new Intent(this, OptionsActivity.class);
                     startActivity(settings);
                     break;
                 case R.id.buttonQuit:
+                    v.startAnimation(AnimationUtils.loadAnimation(this, R.anim.view_clicked));
                     audioPlayer.playAudioFromRaw(R.raw.click);
                     Intent startMain = new Intent(Intent.ACTION_MAIN);
                     startMain.addCategory(Intent.CATEGORY_HOME);
@@ -94,22 +99,26 @@ public class MainMenu extends BaseActivity implements View.OnClickListener {
                     startActivity(startMain);
                     break;
                 case R.id.buttonDebug:
+                    v.startAnimation(AnimationUtils.loadAnimation(this, R.anim.view_clicked));
                     audioPlayer.playAudioFromRaw(R.raw.click);
                     findViewById(R.id.buttonMapTest).setVisibility(View.VISIBLE);
                     findViewById(R.id.buttonFirebaseTest).setVisibility(View.VISIBLE);
                     findViewById(R.id.buttonMQTTTest).setVisibility(View.VISIBLE);
                     break;
                 case R.id.buttonMapTest:
+                    v.startAnimation(AnimationUtils.loadAnimation(this, R.anim.view_clicked));
                     audioPlayer.playAudioFromRaw(R.raw.click);
                     Intent mapTest = new Intent(this, MapTestActivity.class);
                     startActivity(mapTest);
                     break;
                 case R.id.buttonMQTTTest:
+                    v.startAnimation(AnimationUtils.loadAnimation(this, R.anim.view_clicked));
                     audioPlayer.playAudioFromRaw(R.raw.click);
                     Intent mqttTest = new Intent(this, MQTTActivity.class);
                     startActivity(mqttTest);
                     break;
                 case R.id.buttonFirebaseTest:
+                    v.startAnimation(AnimationUtils.loadAnimation(this, R.anim.view_clicked));
                     audioPlayer.playAudioFromRaw(R.raw.click);
                     Intent firebaseTest = new Intent(this, FirebaseActivity.class);
                     startActivity(firebaseTest);
