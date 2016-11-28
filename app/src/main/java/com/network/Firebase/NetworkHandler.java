a3edacb32debaa8dd4b51abf582f95b32734ce19
Patrick2990 <Kaalund1993@hotmail.com>
2016-11-28 15:26:22 +0100

    Multiplayer: Added game status on firebase

--- app/src/main/java/com/network/Firebase/NetworkHandler.java
+++ app/src/main/java/com/network/Firebase/NetworkHandler.java
@@ -2,6 +2,7 @@ package com.network.Firebase;
 
 import android.util.Log;
 
+import com.gamelogic.DataContainer;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,26 +23,45 @@ public class NetworkHandler {
     private DatabaseReference mFirebaseDatabaseReference;
     private ArrayList<RemotePlayer> playerListeners;
     private String game;
+    private boolean multiPlayerGame;
 
-    public NetworkHandler(){
+    public enum gameStatus{
+        FILLING_GAME,
+        BEGUN
+    }
+
+    public NetworkHandler(boolean multiPlayerGame) {
         mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
         playerListeners = new ArrayList<>();
 
-        game = "Game";
+        this.multiPlayerGame = multiPlayerGame;
 
-        // Create game on firebase, or empties an existing game for testing :-)
-        mFirebaseDatabaseReference.child(game).removeValue();
+        if(this.multiPlayerGame){
+            // testing - adding games
+            String testGame1 = "Game1";
+            String testGame2 = "Game2";
 
-        Log.d("NetworkHandler", "NetworkHandler started");
+            mFirebaseDatabaseReference.child(testGame1).child("Status").setValue(gameStatus.BEGUN.ordinal());
+            mFirebaseDatabaseReference.child(testGame2).child("Status").setValue(gameStatus.BEGUN.ordinal());
 
-        startListenOnFirebase();
+            game = "Game3";
+
+            // Create game on firebase, or empties an existing game for testing :-)
+            mFirebaseDatabaseReference.child(game).removeValue();
+            mFirebaseDatabaseReference.child(game).setValue("Begun");
+            mFirebaseDatabaseReference.child(game).child("Status").setValue(gameStatus.FILLING_GAME.ordinal());
+
+            Log.d("NetworkHandler", "NetworkHandler started");
+
+            startListenOnFirebase();
+        }
     }
 
-    public void addPlayerListener(RemotePlayer playerListener){
+    public void addPlayerListener(RemotePlayer playerListener) {
         playerListeners.add(playerListener);
     }
 
-    private void startListenOnFirebase(){
+    private void startListenOnFirebase() {
         mFirebaseDatabaseReference.child(game).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -50,18 +70,27 @@ public class NetworkHandler {
                     try {
                         JSONObject jsonObject = new JSONObject(receivedObject.toString());
 //                        Log.d("NetworkHandler", "Received json: " + jsonObject.toString());
-                        Long receivedTime = jsonObject.getLong("Time");
-                        Long xPos = jsonObject.getLong("X");
-                        Long yPos = jsonObject.getLong("Y");
-                        int angle = jsonObject.getInt("Angle");
-                        long time = System.currentTimeMillis() - receivedTime;
+                        Integer gameStatus = jsonObject.getInt("Status");
+                        if (gameStatus == NetworkHandler.gameStatus.FILLING_GAME.ordinal()) {
+//                            Log.e("NetworkHandler", "Game status: " + gameStatus);
+
+                            // start game
+                            beginGame();
+                        }
+                        // Connecting
+                        else {
+                            Long receivedTime = jsonObject.getLong("Time");
+                            Long xPos = jsonObject.getLong("X");
+                            Long yPos = jsonObject.getLong("Y");
+                            int angle = jsonObject.getInt("Angle");
+                            long time = System.currentTimeMillis() - receivedTime;
 //                        mMessageReceivedText.setText("Time delay: " + time + " millis");
 //                        Log.d("NetworkHandler", "Time delay: " + time + " millis");
 
-                        for(RemotePlayer playerListener : playerListeners){
-                            playerListener.updatePlayerPosition(xPos, yPos, angle);
+                            for (RemotePlayer playerListener : playerListeners) {
+                                playerListener.updatePlayerPosition(xPos, yPos, angle);
+                            }
                         }
-
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -82,5 +111,12 @@ public class NetworkHandler {
         mFirebaseDatabaseReference.child(game).child("X").setValue(centerX);
         mFirebaseDatabaseReference.child(game).child("Y").setValue(centerY);
         mFirebaseDatabaseReference.child(game).child("Angle").setValue(angle);
+    }
+
+    public void beginGame(){
+
+        updatePlayerPosition(DataContainer.player.getPos().x, DataContainer.player.getPos().y, 0);
+        mFirebaseDatabaseReference.child(game).child("Status").setValue(NetworkHandler.gameStatus.BEGUN.ordinal());
+
     }
 }
