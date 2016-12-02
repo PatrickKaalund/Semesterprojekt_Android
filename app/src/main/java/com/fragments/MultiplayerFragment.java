b419439d290a1b837c26d7a0987b68ab0afba9dc
Patrick2990 <Kaalund1993@hotmail.com>
2016-12-02 18:42:16 +0100

    Multiplayer fragment v1

--- app/src/main/java/com/fragments/MultiplayerFragment.java
+++ app/src/main/java/com/fragments/MultiplayerFragment.java
@@ -3,6 +3,7 @@ package com.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,13 +17,10 @@ import com.activities.InGame;
 import com.audio.AudioPlayer;
 import com.example.patrickkaalund.semesterprojekt_android.R;
 
-
 public class MultiplayerFragment extends Fragment implements View.OnClickListener {
 
     View view;
     private AudioPlayer audioPlayer;
-    public int numberOfPlayers = 2;
-    public int scrollYpos = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -39,14 +37,23 @@ public class MultiplayerFragment extends Fragment implements View.OnClickListene
         numberPicker.setMinValue(2);
         numberPicker.setMaxValue(5);
 
-//        enableNumberPickerManualEditing(numberPicker, false);
+        enableNumberPickerManualEditing(numberPicker, false);
 
-        String[] test = { "2 players", "3 players", "4 players", "5 players" };
+        updateView(numberPicker, 2);
 
-        EditText tv1 = (EditText)numberPicker.getChildAt(0);
-        tv1.setTextSize(30);
+        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(android.widget.NumberPicker picker, int oldVal, int newVal) {
+                    updateView(picker, newVal);
+            }
+        });
 
+        numberPicker.setWrapSelectorWheel(true);
+
+        // Empty - showing drawables inside updateView instead. Must be set in order to not showing default numbers
+        String[] test = { " ", " ", " ", " " };
         numberPicker.setDisplayedValues(test);
+
         numberPicker.setSoundEffectsEnabled(true);
 
         audioPlayer = new AudioPlayer(view.getContext());
@@ -55,23 +62,43 @@ public class MultiplayerFragment extends Fragment implements View.OnClickListene
 
         return view;
     }
-//
-//    public void enableNumberPickerManualEditing(NumberPicker numPicker,
-//                                                boolean enable) {
-//        int childCount = numPicker.getChildCount();
-//
-//        for (int i = 0; i < childCount; i++) {
-//            View childView = numPicker.getChildAt(i);
-//
-//            if (childView instanceof EditText) {
-//                EditText et = (EditText) childView;
-//                et.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.multiplayer_2_players));
-//                et.setTextSize(32);
-//                et.setFocusable(enable);
-//                return;
-//            }
-//        }
-//    }
+
+
+    private void updateView(View view, int newVal){
+//        Log.d("MultiplayerFragment", "Updating! New val: " + newVal);
+
+        switch (newVal){
+            case 2:
+                view.setBackground(ContextCompat.getDrawable(this.getContext().getApplicationContext(), R.drawable.multiplayer_2_player));
+                break;
+            case 3:
+                view.setBackground(ContextCompat.getDrawable(this.getContext().getApplicationContext(), R.drawable.multiplayer_3_player));
+                break;
+            case 4:
+                view.setBackground(ContextCompat.getDrawable(this.getContext().getApplicationContext(), R.drawable.multiplayer_4_player));
+                break;
+            case 5:
+                view.setBackground(ContextCompat.getDrawable(this.getContext().getApplicationContext(), R.drawable.multiplayer_5_player));
+                break;
+            default:
+                Log.e("MultiplayerFragment", "In default mode!!");
+        }
+    }
+
+
+    public void enableNumberPickerManualEditing(NumberPicker numPicker,
+                                                boolean enable) {
+        int childCount = numPicker.getChildCount();
+
+        for (int i = 0; i < childCount; i++) {
+            View childView = numPicker.getChildAt(i);
+
+            if (childView instanceof EditText) {
+                EditText et = (EditText) childView;
+                et.setFocusable(enable);
+            }
+        }
+    }
 
 
     @Override
