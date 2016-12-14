68017a8e99f128e4329563faf7216781a7372302
Hippomormor <cgenter@oncable.dk>
2016-12-14 11:51:47 +0100

    Added comments

--- app/src/main/java/com/fragments/EndScreenFragment.java
+++ app/src/main/java/com/fragments/EndScreenFragment.java
@@ -42,6 +42,7 @@ public class EndScreenFragment extends Fragment implements View.OnClickListener 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        // Get bundle args
         Bundle bundle = this.getArguments();
         if (bundle != null) {
             shotsFired = bundle.getInt("shots");
@@ -49,9 +50,11 @@ public class EndScreenFragment extends Fragment implements View.OnClickListener 
             hits = bundle.getInt("hits");
         }
 
+        // Inflate view
         View view = inflater.inflate(R.layout.fragment_end_game, container, false);
         this.view = view;
 
+        // Create new glSurfaceView
         glSurfaceView = new OurGLSurfaceView(getActivity().getApplicationContext());
 
         // Inflate glSurfaceView on top of running activity
@@ -59,24 +62,31 @@ public class EndScreenFragment extends Fragment implements View.OnClickListener 
                 ViewGroup.LayoutParams.MATCH_PARENT));
         glSurfaceView.setZOrderOnTop(true);
 
+        // Set onClickLIsteners
         TextView nextButton = (TextView) view.findViewById(R.id.buttonNext);
-
         nextButton.setOnClickListener(this);
 
+        // Create audioplayer
         audioPlayer = new AudioPlayer(view.getContext());
 
+        // Bring view to front
         view.bringToFront();
 
+        // Get displaymetrics
         displayMetrics = view.getResources().getDisplayMetrics();
+
+        // Create spritefactory
         endScreenFactory = new SpriteEntityFactory(R.drawable.numbers_fps, 120, 120, 11, 1, new PointF(0, 0));
 
         return view;
     }
 
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        // Create & start drawers
         killDrawers = new ArrayList<>();
         hitsDrawers = new ArrayList<>();
         shotsDrawers = new ArrayList<>();
@@ -85,12 +95,15 @@ public class EndScreenFragment extends Fragment implements View.OnClickListener 
         startDrawers(shotsDrawers, 110);
         startDrawers(hitsDrawers, 220);
 
+        // Start drawing thread
         new Thread(background).start();
     }
 
 
     public void startDrawers(ArrayList<Entity> drawers, int yOffset) {
 
+        // Place each drawer on same x-coordinate with 60 pixels in between
+        // Use yOffset as y-offset :)
         for (int i = 0; i < 4; i++) {
             Entity drawer = endScreenFactory.createEntity();
             drawer.placeAt(displayMetrics.widthPixels - 300 - (60 * i), displayMetrics.heightPixels - 800 - yOffset);
@@ -102,12 +115,15 @@ public class EndScreenFragment extends Fragment implements View.OnClickListener 
 
     private Runnable background = new Runnable() {
 
+        // Wait for 600millis and draw numbers
         private void draw(ArrayList<Entity> drawers, int size) throws InterruptedException {
             Thread.sleep(600);
 
+            // Get length of number to draw
             String sizeString =  Integer.toString(size);
             if (size < 9999 && size > 0) {
 
+                // Draw each number separately
                 for (int i = 0; i < sizeString.length(); i++) {
                     // get next digit in string  - convert from ASCII char
                     int sprite = sizeString.charAt(sizeString.length() - (i + 1)) - 48;
@@ -115,6 +131,7 @@ public class EndScreenFragment extends Fragment implements View.OnClickListener 
                     for (int j = 0; j < sprite; j++) {
                         drawers.get(i).setCurrentSprite(j + 1);
 
+                        // Request redraw and play click-sound then wait for 300millis
                         audioPlayer.playAudioFromRaw(R.raw.click);
                         glSurfaceView.requestRender();
                         Thread.sleep(350);
@@ -125,6 +142,7 @@ public class EndScreenFragment extends Fragment implements View.OnClickListener 
 
         @Override
         public void run() {
+            // Request drawing from background thread
             try {
                 draw(killDrawers, kills);
                 draw(shotsDrawers, shotsFired);
@@ -135,25 +153,22 @@ public class EndScreenFragment extends Fragment implements View.OnClickListener 
         }
     };
 
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonNext:
                 Log.d("Main", "Clicked");
+                // Run animation now to match time with fragment removal
                 v.startAnimation(AnimationUtils.loadAnimation(view.getContext(), R.anim.view_clicked));
 
                 // Delete sprite factory and remove glSurfaceView
                 endScreenFactory.delete();
                 glSurfaceView.setVisibility(View.INVISIBLE);
 
+                // Play click
                 audioPlayer.playAudioFromRaw(R.raw.click);
 
+                // Remove this fragment from stack and put high-score fragment on stack
                 getFragmentManager().beginTransaction()
                         .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
                         .remove(this)
